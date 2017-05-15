21f945b99d19722d5162db8f84b1ed0f2b69ac16
StraetenMax <maxstraeten@yahoo.com>
2017-05-15 08:16:56 +0200

    proprietes et constructeurs

--- src/originePoint/Point.java
+++ src/originePoint/Point.java
@@ -1,5 +1,18 @@
 package originePoint;
 
 public class Point {
-
+	//propriétés
+	private double abscisse;
+	private double ordonne;
+	
+	//constructeurs
+	public Point(){
+		abscisse = 00.00;
+		ordonne = 00.00;
+	}
+	public Point(double abscisse, double ordonne){
+		this.abscisse = abscisse;
+		this.ordonne = ordonne;
+	}
+	
 }
