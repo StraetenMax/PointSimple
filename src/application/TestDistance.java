6776ae524875d4af10d5dd677f252e98547b5c93
StraetenMax <maxstraeten@yahoo.com>
2017-05-15 10:18:18 +0200

    class TestDistance

--- src/application/TestDistance.java
+++ src/application/TestDistance.java
@@ -7,15 +7,19 @@ public class TestDistance {
 
 	public static void main(String[] args) {
 		Point[] points = new Point[3];
-		for(int i =0; i< points.length; i++ ){	
 		Scanner clavier = new Scanner(System.in);
-		System.out.println("la position "+(i+1)+" de x est :");
-		double abscisse = clavier.nextDouble();
-		System.out.println("la position "+(i+1)+" de Y est :");
-		double ordonne = clavier.nextDouble();
-		points[i]= new Point(abscisse, ordonne);
+		for (int i = 0; i < points.length; i++) {	
+			System.out.print("la position " + (i + 1) + " de x est :");
+			double abscisse = clavier.nextDouble();
+			System.out.print("la position " + (i + 1) + " de Y est :");
+			double ordonne = clavier.nextDouble();
+			points[i] = new Point(abscisse, ordonne);
 		}
-		//clavier.close();
-
-}
+		clavier.close();
+		
+		for(Point dist : points)
+		System.out.println(dist);
+		
+		
+	}
 }
